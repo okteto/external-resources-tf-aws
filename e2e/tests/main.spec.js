acd533e1f30aee933112bc1eee6cb9c155e4960f
Ramiro <ramiro@okteto.com>
2024-11-25 21:57:38 -0800

    hardcoded url

--- e2e/tests/main.spec.js
+++ e2e/tests/main.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('https://menu-oktaco-pr-20.demo.okteto.dev');
 
   // The page title
   await expect(page).toHaveTitle('The Oktaco Shop');
@@ -9,7 +9,7 @@ test('has title', async ({ page }) => {
 
 
 test('menu input has correct placeholder', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('https://menu-oktaco-pr-20.demo.okteto.dev');
 
   const menuInput = await page.locator('input#item');
   await expect(menuInput).toBeVisible();
