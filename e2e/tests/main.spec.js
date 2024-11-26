1175c69dd779b9bb9a06403c7848dd10e63ef737
Ramiro <ramiro@okteto.com>
2024-11-25 22:04:13 -0800

    add test to check for env vars

--- e2e/tests/main.spec.js
+++ e2e/tests/main.spec.js
@@ -1,5 +1,13 @@
 import { test, expect } from '@playwright/test';
 
+test('environment variables are set', async () => {
+  expect(process.env.OKTETO_NAMESPACE).toBeDefined();
+  expect(process.env.OKTETO_DOMAIN).toBeDefined();
+  expect(process.env.OKTETO_NAMESPACE).not.toBe('');
+  expect(process.env.OKTETO_DOMAIN).not.toBe('');
+});
+
+
 test('has title', async ({ page }) => {
   await page.goto('https://menu-oktaco-pr-20.demo.okteto.dev');
 
